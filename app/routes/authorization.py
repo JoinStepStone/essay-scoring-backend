634215a68771184d67bd4f3d6acc29a014b4be99
Nauman Ahmed <ahmedNauman97>
2024-08-16 17:13:04 +0200

    updated auth route

--- app/routes/authorization.py
+++ app/routes/authorization.py
@@ -2,6 +2,10 @@ from flask import request, jsonify
 from app import app, db
 from ..controller.authorization import signUpController, signInController
 import os
+from datetime import datetime
+import json
+from bson import json_util, Timestamp
+from bson.objectid import ObjectId
 
 @app.route('/', methods=['GET'])
 def Home():
@@ -23,12 +27,22 @@ def login():
         return jsonify({"data": response, "code": 201, "message": message})
     return jsonify({"error": response, "code": 400, "message": message})
 
+def custom_json_serializer(obj):
+    if isinstance(obj, ObjectId):
+        return str(obj)
+    elif isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    elif isinstance(obj, Timestamp):
+        return str(obj)
+    # Add other non-serializable types here as needed
+    raise TypeError(f"Type {type(obj)} not serializable")
+
 @app.route('/test_connection', methods=['GET'])
 def test_connection():
     try:
         print("\n", "TESTING", "\n")
         # Perform a test query to check the connection
         server_status = db.command("serverStatus")
-        return jsonify({"status": "Connection successful", "code": 200, "server_info": server_status})
+        return jsonify({"status": "Connection successful", "code": 200, "server_info": json.loads(json_util.dumps(server_status))})
     except Exception as e:
         return jsonify({"status": "Connection failed", "code": 500, "error": str(e)})
