b553845bb139c2b717f3d9ff7ea1f5b244cbe462
Nauman Ahmed <ahmedNauman97>
2024-08-16 17:05:04 +0200

    updated the os

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
 from gridfs import GridFS
+import os
 
 UPLOAD_FOLDER = './app/uploads/'
 app = Flask(__name__)
 CORS(app)
 app.config["SECRET_KEY"] = "db24c608640f5034b30b8e1e1eb5618ed0ffdbf5"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/todo_db"
+app.config["MONGO_URI"] = os.environ.get('MONGODB_URI')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # del app.config["MONGO_URI"]
